49dfe2287fbd7e744c54ee8de32e1602f5d74f68
Emirhan Uyunmaz <emirhanuyunmaz46@gmail.com>
2025-05-15 20:31:18 +0300

    initial commit

---
+++ fe/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from 'antd'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <Button color='primary' variant='filled'>HELLO</Button>
+    
+    </>
+  )
+}
+
+export default App
